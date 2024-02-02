4565a3f2b6f917c5f6786e1794b3b98dcb6e8fca
AnthonySaDev <anthonysareis11@gmail.com>
2024-02-02 16:37:10 -0300

    add: tests created for starRating component

--- src/components/StarRating/__tests__/StarRating.test.tsx
+++ src/components/StarRating/__tests__/StarRating.test.tsx
@@ -3,8 +3,23 @@ import {render} from '@testing-library/react-native';
 import {StarRating} from '../StarRating';
 
 describe('StarRating', () => {
-  test('o componente renderizou corretamente', () => {
-   const {debug} = render(<StarRating rating={{average: 5}} />);
-   debug();
+  describe('Se eu passar a nota', () => {
+    it('mostrar a nota', () => {
+      const {getByText} = render(<StarRating rating={{average: 5}} />);
+
+      expect(getByText('5')).toBeTruthy();
+    });
+    it('mostrar o icone', () => {
+      const {getByTestId} = render(<StarRating rating={{average: 5}} />);
+
+      expect(getByTestId('starIcon')).toBeTruthy();
+    });
   });
+
+  describe('Se eu não passar a nota', () => {
+    it('mostrar nada', ()=>{
+      const { UNSAFE_root: root } = render(<StarRating />);
+      expect(root.children.length).toEqual(0); 
+    } )
+  })
 });
